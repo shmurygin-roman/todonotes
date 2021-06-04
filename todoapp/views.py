66b88ea373c5502e2312e18c6bfc140b1e05f437
Unknown <unknown@example.com>
2021-06-04 07:05:52 -0700

    Lesson 7

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,7 +3,7 @@ from rest_framework.renderers import (BrowsableAPIRenderer, HTMLFormRenderer,
 from rest_framework.viewsets import ModelViewSet
 #from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import *
 from .serializers import *
 
@@ -15,7 +15,8 @@ from .serializers import *
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-#    pagination_class = ProjectLimitOffsetPagination
+    #pagination_class = ProjectLimitOffsetPagination
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         queryset = Project.objects.all()
@@ -32,8 +33,9 @@ class ProjectModelViewSet(ModelViewSet):
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
-#    pagination_class = ToDoLimitOffsetPagination
+    #pagination_class = ToDoLimitOffsetPagination
     filterset_fields = ['project']
+    permission_classes = [permissions.IsAuthenticated]
 
     def destroy(self, request, pk=None):
         try:
